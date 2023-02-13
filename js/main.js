64f174345781629209e342f0c1fa653f89da2679
developer_azamat <azamatjonazimov561@gmail.com>
2023-02-13 22:55:22 +0500

    already change

--- js/main.js
+++ js/main.js
@@ -26,20 +26,20 @@ setInterval(()=>{
 
 
 
-// const text = document.querySelector('.text');
-// const loader = document.querySelector('.loader');
-// let txt = 0
+const text = document.querySelector('.text');
+const loader = document.querySelector('.loader');
+let txt = 0
 
-// setTimeout(()=>{
-//     loader.style.display = 'none';
-//     const interval = setInterval(function (){
-//         txt++;
-//         text.textContent = txt
-//     }, 1000)
-//     setTimeout(()=>{
-//         clearInterval(interval);
-//     },10000)
-// },5000)
+setTimeout(()=>{
+    loader.style.display = 'none';
+    const interval = setInterval(function (){
+        txt++;
+        text.textContent = txt
+    }, 1000)
+    setTimeout(()=>{
+        clearInterval(interval);
+    },10000)
+},5000)
 
 
 
