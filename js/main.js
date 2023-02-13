4601b0519effe5e3b1bb189d8db9309be6589742
developer_azamat <azamatjonazimov561@gmail.com>
2023-02-13 19:03:30 +0500

    loader

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 const text = document.querySelector('.text');
+const loader = document.querySelector('.loader');
 let txt = 0
-const interval = setInterval(function (){
-    txt++;
-    text.textContent = txt
-}, 1000)
 
 setTimeout(()=>{
-    clearInterval(interval)
-},10000)
+    loader.style.display = 'none';
+    const interval = setInterval(function (){
+        txt++;
+        text.textContent = txt
+    }, 1000)
+    setTimeout(()=>{
+        clearInterval(interval);
+    },10000)
+},5000)
